6cdfc6b3160cbdb24db58c0df0b2830d2f78c3fb
Kajalti Sirame <kajalti@29>
2026-03-09 02:01:15 +0530

    fixed routing

--- server.js
+++ server.js
@@ -4,18 +4,35 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-/* MAIN PAGE */
+/* MAIN HOMEPAGE */
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 /* TASK ROUTES */
-app.use("/task1-html-server", express.static(path.join(__dirname, "task1-html-server")));
-app.use("/task2-form-validation", express.static(path.join(__dirname, "task2-form-validation")));
-app.use("/task3-responsive-design", express.static(path.join(__dirname, "task3-responsive-design")));
-app.use("/task4-dom-validation", express.static(path.join(__dirname, "task4-dom-validation")));
-app.use("/task5-api-crud", express.static(path.join(__dirname, "task5-api-crud")));
-app.use("/task7-external-api", express.static(path.join(__dirname, "task7-external-api")));
+app.get("/task1", (req, res) => {
+  res.sendFile(path.join(__dirname, "task1-html-server/index.html"));
+});
+
+app.get("/task2", (req, res) => {
+  res.sendFile(path.join(__dirname, "task2-form-validation/index.html"));
+});
+
+app.get("/task3", (req, res) => {
+  res.sendFile(path.join(__dirname, "task3-responsive-design/index.html"));
+});
+
+app.get("/task4", (req, res) => {
+  res.sendFile(path.join(__dirname, "task4-dom-validation/index.html"));
+});
+
+app.get("/task5", (req, res) => {
+  res.sendFile(path.join(__dirname, "task5-api-crud/index.html"));
+});
+
+app.get("/task7", (req, res) => {
+  res.sendFile(path.join(__dirname, "task7-external-api/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
