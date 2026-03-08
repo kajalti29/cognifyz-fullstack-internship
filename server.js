9acd7f09d160221337bcc197d1d8c87d6bcef5dd
Kajalti Sirame <kajalti@29>
2026-03-09 01:56:28 +0530

    fix routing

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-/* MAIN HOME PAGE */
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-/* TASK FOLDERS */
+// TASK FOLDERS
 app.use("/task1-html-server", express.static(path.join(__dirname, "task1-html-server")));
 app.use("/task2-form-validation", express.static(path.join(__dirname, "task2-form-validation")));
 app.use("/task3-responsive-design", express.static(path.join(__dirname, "task3-responsive-design")));
@@ -17,6 +12,11 @@ app.use("/task4-dom-validation", express.static(path.join(__dirname, "task4-dom-
 app.use("/task5-api-crud", express.static(path.join(__dirname, "task5-api-crud")));
 app.use("/task7-external-api", express.static(path.join(__dirname, "task7-external-api")));
 
+// MAIN HOME PAGE
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
