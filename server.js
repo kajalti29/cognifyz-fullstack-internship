4508c4beed365b575462930809e7512a542f6f42
Kajalti Sirame <kajalti@29>
2026-03-09 01:50:08 +0530

    fixed task links

--- server.js
+++ server.js
@@ -3,19 +3,15 @@ const path = require("path");
 
 const app = express();
 
-app.use("/task1", express.static(path.join(__dirname, "task1-html-server/public")));
-app.use("/task2", express.static(path.join(__dirname, "task2-form-validation/public")));
-app.use("/task3", express.static(path.join(__dirname, "task3-responsive-design/public")));
-app.use("/task4", express.static(path.join(__dirname, "task4-dom-validation/public")));
-app.use("/task5", express.static(path.join(__dirname, "task5-api-crud/public")));
-app.use("/task7", express.static(path.join(__dirname, "task7-external-api/public")));
+app.use("/task1-html-server", express.static(path.join(__dirname,"task1-html-server")));
+app.use("/task2-form-validation", express.static(path.join(__dirname,"task2-form-validation")));
+app.use("/task3-responsive-design", express.static(path.join(__dirname,"task3-responsive-design")));
+app.use("/task4-dom-validation", express.static(path.join(__dirname,"task4-dom-validation")));
+app.use("/task5-api-crud", express.static(path.join(__dirname,"task5-api-crud")));
+app.use("/task7-external-api", express.static(path.join(__dirname,"task7-external-api")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.use(express.static(__dirname));
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.listen(3000,()=>{
+console.log("Server running on port 3000");
 });
